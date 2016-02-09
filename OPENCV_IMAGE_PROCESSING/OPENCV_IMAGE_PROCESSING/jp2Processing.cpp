2e58e709a57fe36f78e813a88dee978da29a4482
Tsosie Ernest-Andrew Schneider <ktaed@nau.edu>
2016-02-09 10:27:40 -0800

    Stop Gap Solution to merge training data

--- OPENCV_IMAGE_PROCESSING/OPENCV_IMAGE_PROCESSING/jp2Processing.cpp
+++ OPENCV_IMAGE_PROCESSING/OPENCV_IMAGE_PROCESSING/jp2Processing.cpp
@@ -1,12 +1,8 @@
+#define MS_NO_COREDLL
+
 #include "gdal_priv.h"
 #include "cpl_conv.h" // for CPLMalloc()
-
-
-#include "opencv2/core/core.hpp"
-//#include "opencv2/flann/miniflann.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-//#include "opencv2/photo/photo.hpp"
-//#include "opencv2/video/video.hpp"
+#include "Python.h"
 //#include "opencv2/features2d/features2d.hpp"
 //#include "opencv2/objdetect/objdetect.hpp"
 //#include "opencv2/calib3d/calib3d.hpp"
@@ -18,40 +14,134 @@
 //#include "opencv2/highgui/highgui_c.h"
 //#include "opencv2/imgproc/imgproc_c.h"
 #include <iostream>
+#include <string.h>
+//using namespace cv;
+//using namespace std;
 
-using namespace cv;
-using namespace std;
+GDALDataset* open_dataset(std::string file_name) {
+	return (GDALDataset *)GDALOpen(file_name.c_str(), GA_Update);
+}
 
-int main()
-{
-	GDALDataset  *dataset;
-	GDALRasterBand *band, *overview;
+uint8_t* get_mask(uint8_t* origin, uint8_t* labelled, uint32_t length) {
 
-	//registers drivers
-	GDALAllRegister();
-	//GDALOpen() function to open a datset, in this case a JP2 file
-	dataset = (GDALDataset *)GDALOpen("example2.jp2", GA_ReadOnly);
+	for (uint32_t i = 0; i < length; i++)
+		if (origin[i] == labelled[i])
+			labelled[i] = 0;
+		else
+			labelled[i] = 255;
 
-	band = dataset->GetRasterBand(1);
-	overview = band->GetOverview(3);
+	return labelled;
+}
 
-	//	int block_x, block_y;
+bool get_north(uint8_t* image, uint32_t position, int64_t width) {
+	int64_t test = (position - width);
+	if ( test< 0)
+		return false;
+	else if (image[position] == image[position - width])
+		return true;
+	else
+		return false;
+}
+bool get_south(uint8_t* image, uint32_t position, uint32_t width,uint32_t height) {
+	if (position + width >= width*height)
+		return false;
+	else if (image[position] == image[position + width])
+		return true;
+	else
+		return false;
+}
+bool get_west(uint8_t* image, uint32_t position) {
+	if (position - 1 < 0)
+		return false;
+	else if (image[position] == image[position - 1])
+		return true;
+	else
+		return false;
+}
+bool get_east(uint8_t* image, uint32_t position, uint32_t width, uint32_t height) {
+	if (position + 1 <= width*height)
+		return false;
+	else if (image[position] == image[position + 1])
+		return true;
+	else
+		return false;
+}
 
-	//	overview->GetBlockSize(&block_x, &block_y);
 
-	uint image_width = overview->GetXSize();
-	uint image_height = overview->GetYSize();
-	//print out image_width and image_height to console
-	cout << image_width << " " << image_height << " " << dataset << endl;
+uint8_t* remove_noise(uint8_t* mask_band, uint32_t width, uint32_t height) {
+	for (uint32_t i = 0; i < width*height; i++)
+		if (!(get_north(mask_band, i, width) || get_south(mask_band, i, width, height) || get_east(mask_band, i, width, height) || get_west(mask_band, i)))
+			mask_band[i] = 255;
+	return mask_band;
+}
 
-	ushort * arr = (ushort *)VSIMalloc(sizeof(ushort)*image_width*image_height);//new uint16_t[image_width];//(uint16_t *)VSIMalloc(image_length);
-	overview->RasterIO(GF_Read, 0, 0, image_width, image_height, arr, image_width, image_height, GDT_UInt16, 0, 0);
-	Mat image = Mat(image_height, image_width, CV_16U, arr);
-	cv::imwrite("C:\\warmerda\\bld\\bin\\downsampled.jp2", image);
-	VSIFree(arr);
+void shift_left(uint16_t* input, uint32_t length) {
+	for (uint32_t i = 0; i < length; i++)
+		input[i] = input[i] << 8;
+	return;
+}
+
+int main(){
+	std::string src = "PSP_009650_1755_RED.tif";
+	std::string src_lbl = "PSP_009650_1755_RED_dunes.tif";
+
+		GDALDataset  *dataset, *mask;
+		GDALRasterBand *data_band, *mask_band;
+		PyObject *toPy;
+		cv::Mat image, mask_layer;
+
+		//registers drivers
+		GDALAllRegister();
+		//GDALOpen() function to open a datset, in this case a JP2 file
+		image = cv::imread(src_lbl + "training_image.tif");
+		if (image.empty()) {
+			dataset = open_dataset(src);
+			mask = open_dataset(src_lbl);
+
+			data_band = dataset->GetRasterBand(1);
+			mask_band = mask->GetRasterBand(1);
 
 
-	GDALClose(dataset);
+			//int block_x, block_y;
+			//data_band->GetBlockSize(&block_x, &block_y);
 
+			uint32_t image_width = data_band->GetXSize();
+			uint32_t image_height = data_band->GetYSize();
+
+			//print out image_width and image_height to console
+
+			uint8_t * data_arr = (uint8_t *)VSIMalloc(sizeof(uint8_t)*image_width*image_height);
+			uint8_t * mask_arr = (uint8_t *)VSIMalloc(sizeof(uint8_t)*image_width*image_height);
+
+			data_band->RasterIO(GF_Read, 0, 0, image_width, image_height, data_arr, image_width, image_height, GDT_Byte, 0, 0);
+			mask_band->RasterIO(GF_Read, 0, 0, image_width, image_height, mask_arr, image_width, image_height, GDT_Byte, 0, 0);
+
+
+			image = cv::Mat(image_height, image_width, CV_8U, data_arr);
+			mask_layer = cv::Mat(image_height, image_width, CV_8U, mask_arr);
+
+			cv::bitwise_xor(image, mask_layer, mask_layer);
+
+
+			std::vector<cv::Mat> matrix_list;
+
+			matrix_list.push_back(image);
+			matrix_list.push_back(mask_layer);
+			matrix_list.push_back(cv::Mat(image_height, image_width, CV_8U, cvScalar(0)));
+			cv::merge(matrix_list, image);
+
+
+			cv::imwrite(src_lbl + "training_image.tif", image);
+			
+			VSIFree(mask_arr);
+			GDALClose(mask);
+
+			VSIFree(data_arr);
+
+			GDALClose(dataset);
+		}
+		else {
+			std::cout << "Place Holder\n";
+		}
 	return 0;
 }
