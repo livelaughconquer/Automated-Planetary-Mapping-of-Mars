35b0b068e41b82b6f92d75f8ddbe5d4118d824cc
Tsosie Ernest-Andrew Schneider <ktaed@nau.edu>
2016-02-09 13:39:54 -0800

    Added comments to demo code

--- OPENCV_IMAGE_PROCESSING/OPENCV_IMAGE_PROCESSING/jp2Processing.cpp
+++ OPENCV_IMAGE_PROCESSING/OPENCV_IMAGE_PROCESSING/jp2Processing.cpp
@@ -42,6 +42,7 @@ bool get_north(uint8_t* image, uint32_t position, int64_t width) {
 	else
 		return false;
 }
+
 bool get_south(uint8_t* image, uint32_t position, uint32_t width,uint32_t height) {
 	if (position + width >= width*height)
 		return false;
@@ -82,62 +83,84 @@ void shift_left(uint16_t* input, uint32_t length) {
 }
 
 int main(){
-	std::string src = "PSP_009650_1755_RED.tif";
-	std::string src_lbl = "PSP_009650_1755_RED_dunes.tif";
+		//Hardcoded strings for training datasets. In future would be parameters for extension.
+		std::string src = "PSP_009650_1755_RED.tif";
+		std::string src_lbl = "PSP_009650_1755_RED_dunes.tif";
 
-		GDALDataset  *dataset, *mask;
+
+		//Create pointers to gdal datasets original, and the one marked by sponsor
+		GDALDataset  *dataset, *mask; 
+
+		//Create pointers to raster bands in dataset
 		GDALRasterBand *data_band, *mask_band;
+
+		//Pointer that will be used to pass data to python interpreter currently only used to make sure library/header was found.
 		PyObject *toPy;
+
+		//OpenCV matrices will be used for image processing
 		cv::Mat image, mask_layer;
 
-		//registers drivers
+		//registers drivers for gdal to use
 		GDALAllRegister();
-		//GDALOpen() function to open a datset, in this case a JP2 file
+
+		//Attempts to load previously built training data if no file found a empty matrix is returned 
 		image = cv::imread(src_lbl + "training_image.tif");
+		
+		//Stop gap solution for merging of training data images housed inside conditional as the processing takes 6 to 7mins
+		//while a load of previously written data is about 2 mins
 		if (image.empty()) {
+
+			// Gets pointers to the two images provided by sponsor to use as training data
 			dataset = open_dataset(src);
 			mask = open_dataset(src_lbl);
 
+			// Acquires pointers to raster data inside dataset structure
 			data_band = dataset->GetRasterBand(1);
 			mask_band = mask->GetRasterBand(1);
 
-
-			//int block_x, block_y;
-			//data_band->GetBlockSize(&block_x, &block_y);
-
+			// Get size of images. In future the sizes should be be acquired from both images and compared to prevent merge errors
 			uint32_t image_width = data_band->GetXSize();
 			uint32_t image_height = data_band->GetYSize();
-
-			//print out image_width and image_height to console
-
+			
+			//Allocates arrays to store image information for original and labelled images
+			//Initial trainng data is known to be 8-bit but future should check the data-type of image and process accordingly
 			uint8_t * data_arr = (uint8_t *)VSIMalloc(sizeof(uint8_t)*image_width*image_height);
 			uint8_t * mask_arr = (uint8_t *)VSIMalloc(sizeof(uint8_t)*image_width*image_height);
 
+
+			//Read entire images into their respective arrays
 			data_band->RasterIO(GF_Read, 0, 0, image_width, image_height, data_arr, image_width, image_height, GDT_Byte, 0, 0);
 			mask_band->RasterIO(GF_Read, 0, 0, image_width, image_height, mask_arr, image_width, image_height, GDT_Byte, 0, 0);
 
 
+			//Builds a opencv matrix out of the image data housed in the allocated arrays
 			image = cv::Mat(image_height, image_width, CV_8U, data_arr);
 			mask_layer = cv::Mat(image_height, image_width, CV_8U, mask_arr);
 
+			//xor the matrices to find differences between images provided by sponsor
 			cv::bitwise_xor(image, mask_layer, mask_layer);
 
-
+			//Set up vector to use as a list of matrices
 			std::vector<cv::Mat> matrix_list;
 
+			//adds matrices to list to represent channels in RGB image
+			//A matrix of zeroes added as padding as 2 channelled image disallowed
 			matrix_list.push_back(image);
 			matrix_list.push_back(mask_layer);
 			matrix_list.push_back(cv::Mat(image_height, image_width, CV_8U, cvScalar(0)));
+			
+			//merges the matrices into one matrix 
 			cv::merge(matrix_list, image);
 
-
+			//write the composite training image as stop gap to load up training data.
 			cv::imwrite(src_lbl + "training_image.tif", image);
-			
+
+			//deallocates the array for labelled image and closes the gdal dataset
 			VSIFree(mask_arr);
 			GDALClose(mask);
 
+			//deallocates the array for labelled image and closes the gdal dataset
 			VSIFree(data_arr);
-
 			GDALClose(dataset);
 		}
 		else {
