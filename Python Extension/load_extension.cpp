cee7ab2f91ecb875f39266f3777eb103c46e2ee1
Tsosie Ernest-Andrew Schneider <ktaed@nau.edu>
2016-04-06 23:47:01 -0700

    getImage can now handle 16bit numpy

--- Python Extension/load_extension.cpp
+++ Python Extension/load_extension.cpp
@@ -103,15 +103,23 @@ void init_extent() {
 }
 
 /*Threading helper function that populates numpy array with pixel data*/
-void filler( void* start, uint8_t * arr,int stride ){
-   uint8_t* temp =  (uint8_t *) start;
+void filler( uint8_t* temp, uint8_t * arr,int stride ){
+   //uint8_t* temp =  (uint8_t *) start;
 //   uint8_t
    for(int i =0; i<stride; i++){
         temp[i] = arr[i];
    }
 //   return;
 }
-
+/*Threading helper function that populates numpy array with pixel data*/
+void filler_16(uint16_t* temp, uint16_t * arr, int stride) {
+	//uint16_t* temp = (uint16_t *)start;
+	//   uint8_t
+	for (int i = 0; i<stride; i++) {
+		temp[i] = arr[i];
+	}
+	//   return;
+}
 /*This returns a bool to indicate that the operation was completed successfully
 	@ PyObject* np: This is an numpy array to be filled with the label information at the pixel level
 	@ string file_name: This is the file system name of image that the training image will be compared too
@@ -204,56 +212,84 @@ bool getTraining(PyObject* np, std::string file_name, std::string training_file)
 	@ string file_name: This is the file system name of image that is to be loaded in the memory
 */
 bool getImage(PyObject* np, std::string file_name){
-    Py_BEGIN_ALLOW_THREADS;
-
+	bool status = true, flag = true;
+	Py_BEGIN_ALLOW_THREADS;
+	
     int min = 2, max =2;
     PyArray_Descr* dtype = NULL;
 
    PyArrayObject* ret = (PyArrayObject*)PyArray_FromAny( np, dtype, min, max, NPY_ARRAY_WRITEABLE, NULL);
-
+   dtype = PyArray_DESCR(ret);
     int dim = PyArray_NDIM(ret);
     npy_intp* test = PyArray_DIMS(ret);
-    uint8_t* tester = (uint8_t*)PyArray_DATA(ret);
-    npy_intp s = PyArray_STRIDE (ret, 0);
+ //   uint8_t* tester = (uint8_t*)PyArray_DATA(ret);
+//    npy_intp s = PyArray_STRIDE (ret, 0);
     GDALDataset* data = open_dataset(file_name);
     GDALRasterBand *data_band = data->GetRasterBand(1);
+	uint16_t * data_arr_16;
+	uint8_t * data_arr;
+	// Get size of images. In future the sizes should be be acquired from both images and compared to prevent merge errors
+	uint32_t image_columns = data_band->GetXSize();
+	uint32_t image_rows = data_band->GetYSize();
 
+	if (dtype->type_num == NPY_UBYTE) {
+		std::cout << " 8 bit\n";
+		data_arr = (uint8_t *)VSIMalloc(sizeof(uint8_t)*image_columns*image_rows);
+		data_band->RasterIO(GF_Read, 0, 0, image_columns, image_rows, data_arr, image_columns, image_rows, GDT_Byte, 0, 0);
 
+	}	
+	else if (dtype->type_num == NPY_USHORT) {
+		data_arr_16 = (uint16_t *)VSIMalloc(sizeof(uint16_t)*image_columns*image_rows);
+		data_band->RasterIO(GF_Read, 0, 0, image_columns, image_rows, data_arr_16, image_columns, image_rows, GDT_UInt16, 0, 0);
+		flag = false;
+		std::cout << " 16 bit\n";
 
-		// Get size of images. In future the sizes should be be acquired from both images and compared to prevent merge errors
-    uint32_t image_columns = data_band->GetXSize();
-    uint32_t image_rows = data_band->GetYSize();
+	}
+	else {
+		status = false;
+		std::cout << "That numpy data type is not supported\n";
+	//	goto fail;
+	}
 
-    uint8_t * data_arr = (uint8_t *)VSIMalloc(sizeof(uint8_t)*image_columns*image_rows);
-    data_band->RasterIO(GF_Read, 0, 0, image_columns, image_rows, data_arr, image_columns, image_rows, GDT_Byte, 0, 0);
 //
-    close_dataset(data);
+	uint64_t position = 0;
 
-//
-    uint64_t position = 0;
-//    uint8_t * temp;
+	close_dataset(data);
+	uint8_t * temp;
+	uint16_t * temp_16;
 
-std::vector<std::thread> threadder;
-uint8_t * temp;
-// }
-    for(uint64_t i=0; i<image_rows;i++){
-           temp =(uint8_t*)PyArray_GETPTR2(ret,i,0);
-            threadder.push_back(std::thread(filler, temp ,&data_arr[position],image_columns));
-            position += image_columns;
-        }
+	if (status) {
+		std::vector<std::thread> threadder;
+		if (flag) {
 
-for(auto& thread : threadder){
-    thread.join();
-}
-// PyEval_ReleaseLock();
- if(position>=(image_rows*image_columns)){
+			for (uint64_t i = 0; i < image_rows; i++) {
+				temp = (uint8_t*)PyArray_GETPTR2(ret, i, 0);
+				threadder.push_back(std::thread(filler, temp, &data_arr[position], image_columns));
+				position += image_columns;
+			}
+		}
+		else {
 
-//    delete threadder;
-    VSIFree(data_arr);
-    Py_END_ALLOW_THREADS;
+			for (uint64_t i = 0; i < image_rows; i++) {
+				temp_16 = (uint16_t*)PyArray_GETPTR2(ret, i, 0);
+				threadder.push_back(std::thread(filler_16, temp_16, &data_arr_16[position], image_columns));
+				position += image_columns;
+			}
+		}
+		for (auto& thread : threadder) {
+			thread.join();
+		}
+	}
+	if(flag)
+	    VSIFree(data_arr);
+	else
+		VSIFree(data_arr_16);
+
+	Py_END_ALLOW_THREADS;
 //    Py_Finalize();
-    return true;
- }
+    return status;
+
+// }
 
 }
 
