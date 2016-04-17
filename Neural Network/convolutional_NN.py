3eec6c47d7f5119192744a05bf4531d147c45e6c
Jorge <jorgecfelix@gmail.com>
2016-04-17 12:19:45 -0700

    Classification CNN

--- Neural Network/convolutional_NN.py
+++ Neural Network/convolutional_NN.py
@@ -12,8 +12,10 @@ from StringIO import StringIO
 from math import sqrt
 import matplotlib.cm as cm
 import pylab
+import cPickle as pickle
 
 #Lasagne Imports
+import lasagne
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
@@ -49,7 +51,8 @@ def create_image_blocks(blocks, row_pixel, col_pixel,og_row_pixel, og_col_pixel,
     return image_blocks, blocks
 
 
-def get_labeled_data(filename, training_file):
+
+def get_labeled_data(filename, training_file, block_size=28):
     """Read input-array (image) and label-images and return it as list of tuples. """
 
     rows,cols =  load_extension.getDims(filename)
@@ -60,12 +63,24 @@ def get_labeled_data(filename, training_file):
     # x is a dummy to use as a form of error checking will return false on error
     x = load_extension.getImage(image ,filename)
     x = load_extension.getTraining(label_image,filename, training_file)
+    X = []
+    y = []
+    for i in xrange(0,rows,block_size):
+        for j in xrange(0,cols,block_size):
+            try:
+                X.append(image[i:i + block_size, j:j + block_size].reshape(1, block_size * block_size))
+                y.append(int(load_extension.getLabel(label_image[i:i + block_size, j:j + block_size], "1", "0", 0.75)))
+            except ValueError:
+                continue
 
-    #Seperate Image and Label into blocks
+    X = np.array(X).astype(np.float32)
+    label_blocks = np.array(y).astype(np.int32)
+    test_blocks = X.reshape(-1, 1, block_size, block_size)
+#Seperate Image and Label into blocks
     #test_blocks,blocks = create_image_blocks(768, 393,11543,rows,cols,image)
     #label_blocks, blocks = create_image_blocks(768, 393,11543,rows,cols,label_image)
-    test_blocks,blocks = load4d(4096, 8, 8,rows,cols,image)
-    label_blocks, blocks = load4d(4096, 8,8,rows,cols,label_image)
+    # test_blocks,blocks = load4d(4096, 8, 8,rows,cols,image)
+    # label_blocks, blocks = load4d(4096, 8,8,rows,cols,label_image)
     #Used to Write image blocks to folder
     #or i in range(blocks):
          #im = Image.fromarray(test_blocks[i][i])
@@ -86,6 +101,13 @@ def view_data(block_number):
         pylab.imshow(arr, cmap=cm.Greys_r)
     pylab.show()
 
+# Load stored model
+#with open('net.pickle', 'rb') as f:
+ #   net_pretrain = pickle.load(f)
+
+#net_pretrain.max_epochs = 25  # Train the previous model over more epochs
+
+
 def convolutionalNeuralNetwork(epochs):
     net = NeuralNet(
         layers=[ #three layers: Input, hidden, and output
@@ -94,36 +116,48 @@ def convolutionalNeuralNetwork(epochs):
             ('pool1', layers.MaxPool2DLayer),
             ('conv2', layers.Conv2DLayer),
             ('pool2', layers.MaxPool2DLayer),
-            ('conv3', layers.Conv2DLayer),
-            ('pool3', layers.MaxPool2DLayer),
-            ('hidden4', layers.DenseLayer),
-            ('hidden5', layers.DenseLayer),
+            #('conv3', layers.Conv2DLayer),
+            #('pool3', layers.MaxPool2DLayer),
+            ('dropout1', layers.DropoutLayer),
+            ('dense', layers.DenseLayer),
+            ('dropout2', layers.DropoutLayer),
             ('output', layers.DenseLayer),
         ],
 
         #Parameters for the layers
-        input_shape = (None, 1, 8, 8), #input pixels per image  block
-        conv1_num_filters=1,
-        conv1_filter_size=(4, 4),
-        pool1_pool_size=(4, 4),
-        conv2_num_filters=4,
+        #input layer
+        input_shape = (None, 1, 28, 28), #input pixels per image  block
+        #convolutional 1
+        conv1_num_filters=8,
+        conv1_filter_size=(2, 2),
+        conv1_nonlinearity=lasagne.nonlinearities.rectify,
+        conv1_W=lasagne.init.GlorotUniform(),
+        #maxpool 1
+        pool1_pool_size=(2, 2),
+        #convolutional 2
+        conv2_num_filters=8,
         conv2_filter_size=(1, 1),
-        pool2_pool_size=(1, 1),
-        conv3_num_filters=4,
-        conv3_filter_size=(1, 1),
-        pool3_pool_size=(1, 1),
-        hidden4_num_units=4,
-        hidden5_num_units=4,
-        output_num_units=1,
-        output_nonlinearity=None,
-
+        conv2_nonlinearity=lasagne.nonlinearities.rectify,
+        conv2_W=lasagne.init.GlorotUniform(),
+        #maxpool 2
+        pool2_pool_size=(4, 4),
+        #dropout 1
+        dropout1_p=0.25,
+        # dense
+        dense_num_units=1000,
+        dense_nonlinearity=lasagne.nonlinearities.rectify,
+        # dropout2
+        dropout2_p=0.25,
+        # output
+        output_nonlinearity=lasagne.nonlinearities.softmax,
+        output_num_units=2,
 
         #optimization method:
         update = nesterov_momentum,
         update_learning_rate = 0.01,
         update_momentum = 0.9,
-        eval_size = .2,
-        regression = True,
+        #eval_size = .2,
+        #regression = True,
         max_epochs = epochs,
         verbose = 1,
     )
@@ -147,32 +181,54 @@ def load4d(blocks, row_pixel, col_pixel,og_row_pixel, og_col_pixel,image):
     #print image_blocks
     #Return numpy array with Image blocks arrays
     return image_blocks, blocks
+
 #######################################################################################################################
 #######################################################################################################################
 """Below is the implementation of the convolutional neural network using the Lasagne library for python"""
 
 #Step1:Load Data
 #assumes you have Ryans images in the same folder as this script
-filename ="circles.tif"
-training_file = "circles_train.tif"
+filename ="test.tif"
+training_file = "train.tif"
 test_blocks, label_blocks = get_labeled_data(filename, training_file)
-print test_blocks.shape
+X = test_blocks
+print label_blocks
+print test_blocks.shape, label_blocks.shape
 
+#labels = getLabel(training_file)
+#print labels
 #print test_blocks
-print label_blocks.shape
+# print label_blocks.shape
 
 #Reshape data into 2D
 #test_blocks = test_blocks.reshape(-1, 4096, 8, 8)
 #label_blocks = label_blocks.reshape(-1, 4096, 8, 8)
 
-#print test_blocks
+print test_blocks
 
 #Step 2 Create Neural Network with 2 Hidden Layers
-net = convolutionalNeuralNetwork(40)
+net = convolutionalNeuralNetwork(10)
 
 #Step 3 Train Neural Net
-
-#net.summary()
 train = net.fit(test_blocks, label_blocks)
+#import pickle to store neural net training
+#train = net_pretrain.fit(test_blocks, label_blocks) #Train pre-trained model more
+
+#Store the trained model
+#with open('net.pickle', 'wb') as f:
+    #pickle.dump(net, f, -1)
 
 #Step 4 Look at Predictions from neural network
+
+y_pred = net.predict(X)
+
+#Checking to see if predictions detect any sand dunes. Outputs the indice where a 1 is found.
+for i in range(y_pred.shape[0]):
+    if y_pred[i] == 1:
+        print i
+
+    #print y_pred[i]
+
+#Plot
+#im = Image.fromarray(y_pred)
+#im.save("label.tif")
